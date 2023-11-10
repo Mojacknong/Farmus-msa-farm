c3e6256c6924e53d7790a9b9e31532f6df14c952
MinchoGreenT <dldmstjq99@gmail.com>
2023-11-11 02:56:06 +0900

    [feat] 유저 별 모든 채소 id 조회, 채소 별 모든 루틴 조회 쿼리

--- src/main/java/com/example/farmusfarm/domain/veggie/repository/RoutineRepository.java
+++ src/main/java/com/example/farmusfarm/domain/veggie/repository/RoutineRepository.java
@@ -3,5 +3,11 @@ package com.example.farmusfarm.domain.veggie.repository;
 import com.example.farmusfarm.domain.veggie.entity.Routine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface RoutineRepository extends JpaRepository<Routine, Long> {
+
+    // 채소 id로 루틴 전체 조회
+    List<Routine> findAllByVeggieId(Long veggieId);
 }
--- src/main/java/com/example/farmusfarm/domain/veggie/repository/VeggieRepository.java
+++ src/main/java/com/example/farmusfarm/domain/veggie/repository/VeggieRepository.java
@@ -2,6 +2,13 @@ package com.example.farmusfarm.domain.veggie.repository;
 
 import com.example.farmusfarm.domain.veggie.entity.Veggie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface VeggieRepository extends JpaRepository<Veggie, Long> {
+
+    // 유저 별 채소 id 전체 조회
+    @Query("select v.id from veggie v where v.userId = :userId")
+    List<Long> findAllVeggiesIdByUserId(Long userId);
 }
