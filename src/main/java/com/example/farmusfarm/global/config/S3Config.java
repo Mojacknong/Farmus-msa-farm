210f9fb89e00ed9e9d3dc805455a4e38aff864f7
MinchoGreenT <dldmstjq99@gmail.com>
2023-11-16 18:14:19 +0900

    [feat] S3 키설정

--- src/main/java/com/example/farmusfarm/global/config/S3Config.java
+++ src/main/java/com/example/farmusfarm/global/config/S3Config.java
@@ -1,5 +1,7 @@
 package com.example.farmusfarm.global.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +11,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
     @Value("${cloud.aws.region.static}")
     private String region;
 
@@ -17,6 +23,7 @@ public class S3Config {
 
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
                 .build();
     }
 }
