38c7eecfe37fad700423c538afaf673d38e9d03e
MinchoGreenT <dldmstjq99@gmail.com>
2023-11-19 06:39:04 +0900

    [fix] 팜클럽 목록 검색 메서드 변경 get -> post

--- src/main/java/com/example/farmusfarm/domain/challenge/service/MissionPostService.java
+++ src/main/java/com/example/farmusfarm/domain/challenge/service/MissionPostService.java
@@ -1,24 +1,27 @@
 package com.example.farmusfarm.domain.challenge.service;
 
 import com.example.farmusfarm.common.S3Service;
+import com.example.farmusfarm.common.Utils;
 import com.example.farmusfarm.domain.challenge.dto.req.CreateMissionPostRequestDto;
 import com.example.farmusfarm.domain.challenge.dto.res.CreateMissionPostResponseDto;
 import com.example.farmusfarm.domain.challenge.dto.res.LikeMissionPostResponseDto;
-import com.example.farmusfarm.domain.challenge.entity.MissionPost;
-import com.example.farmusfarm.domain.challenge.entity.MissionPostImage;
-import com.example.farmusfarm.domain.challenge.entity.MissionPostLike;
-import com.example.farmusfarm.domain.challenge.entity.Registration;
+import com.example.farmusfarm.domain.challenge.entity.*;
 import com.example.farmusfarm.domain.challenge.repository.MissionPostImageRepository;
 import com.example.farmusfarm.domain.challenge.repository.MissionPostLikeRepository;
 import com.example.farmusfarm.domain.challenge.repository.MissionPostRepository;
 import com.example.farmusfarm.domain.challenge.repository.RegistrationRepository;
+import com.example.farmusfarm.domain.history.dto.req.CreateHistoryClubDetailRequestDto;
+import com.example.farmusfarm.domain.veggie.repository.VeggieRepository;
 import com.example.farmusfarm.domain.veggieInfo.openfeign.CropFeignClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,13 +32,13 @@ public class MissionPostService {
     private final MissionPostImageRepository missionPostImageRepository;
     private final MissionPostLikeRepository missionPostLikeRepository;
     private final RegistrationRepository registrationRepository;
+    private final VeggieRepository veggieRepository;
 
     private final S3Service s3Service;
-    private final ChallengeService challengeService;
 
     private final CropFeignClient cropFeignClient;
 
-    public CreateMissionPostResponseDto createMissionPost(CreateMissionPostRequestDto requestDto, MultipartFile image) {
+    public CreateMissionPostResponseDto createMissionPost(Long userId, CreateMissionPostRequestDto requestDto, MultipartFile image) {
         Registration registration = registrationRepository.findById(requestDto.getRegistrationId())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 등록입니다."));
 
@@ -47,10 +50,15 @@ public class MissionPostService {
         MissionPostImage missionPostImage = MissionPostImage.createMissionPostImage(imageUrl, savedPost);
         missionPostImageRepository.save(missionPostImage);
 
-        // 다음 스텝 불러오기
-        String nextStep = cropFeignClient.getVeggieInfoStepName(registration.getVeggie().getVeggieInfoId(), savedPost.getStep() + 1).getStepName();
-        registration.updateCurrentStep(nextStep);
-        registrationRepository.save(registration);
+        if (registration.getCurrentStep() == registration.getChallenge().getMaxStep() - 1) {
+            completeChallenge(registration, userId);
+        } else {
+            // 다음 스텝 불러오기
+            String nextStep = cropFeignClient.getVeggieInfoStepName(registration.getVeggie().getVeggieInfoId(), savedPost.getStep() + 1).getStepName();
+            registration.updateCurrentStep(nextStep);
+            registrationRepository.save(registration);
+
+        }
 
         return CreateMissionPostResponseDto.of(savedPost.getId(), registration.getChallenge().getChallengeName(), savedPost.getStep(), imageUrl);
     }
@@ -72,5 +80,30 @@ public class MissionPostService {
     public MissionPost getMissionPost(Long missionPostId) {
         return missionPostRepository.findById(missionPostId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 미션 포스트입니다."));
+    }
+
+    public void completeChallenge(Registration registration, Long userId) {
+        // 등록 정보에서 미션 포스트 리스트 돌면서 HistoryClubPost에 저장
+        List<CreateHistoryClubDetailRequestDto.HistoryClubPost> missionPosts = registration.getMissionPosts()
+                .stream()
+                .map(missionPost -> CreateHistoryClubDetailRequestDto.HistoryClubPost.builder()
+                        .stepNum(missionPost.getStep())
+                        .stepName(registration.getCurrentStepName())
+                        .postImage(missionPost.getMissionPostImages().get(0).getImageUrl())
+                        .content(missionPost.getContent())
+                        .dateTime(Utils.dateTimeFormat(missionPost.getCreatedDate()))
+                        .likeNum(missionPost.getMissionPostLikes().size())
+                        .build())
+                .collect(Collectors.toList());
+
+        Challenge challenge = registration.getChallenge();
+        // 히스토리 생성
+//        CreateHistoryClubDetailRequestDto requestDto = new CreateHistoryClubDetailRequestDto(
+//                challenge.getImageUrl(),
+//                challenge.getVeggieName(),
+//                challenge.getChallengeName(),
+//
+//
+//        )
     }
 }
--- src/main/java/com/example/farmusfarm/domain/history/dto/req/CreateHistoryClubDetailRequestDto.java
+++ src/main/java/com/example/farmusfarm/domain/history/dto/req/CreateHistoryClubDetailRequestDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -14,6 +16,8 @@ public class CreateHistoryClubDetailRequestDto {
     private String veggieName;
     private String name;
     private String period;
+
+    private List<HistoryClubPost> missionPosts;
 
     @AllArgsConstructor
     @NoArgsConstructor
